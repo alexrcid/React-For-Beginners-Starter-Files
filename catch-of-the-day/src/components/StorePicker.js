c2856b9e707fc26af7bdb2a7ec644db2d1e8f983
Alex <alexrcid@hotmail.com>
2019-01-08 17:32:20 +0100

    About to start goToStore()

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { getFunName } from "../helpers"
+
+class StorePicker extends React.Component {
+    goToStore(event) {
+        // 1. Stop the form from submitting
+        event.preventDefault();
+        // 2. Get the text from that input
+         
+        // 3. Change the page to store/whatever-they-entered
+    }
+
+    render() {
+        return (
+            <form className="store-selector" onSubmit={this.goToStore}>
+                <h2>Please Enter a Store</h2>
+                <input 
+                    type="text" 
+                    required 
+                    placeholder="Store Name" 
+                    defaultValue={getFunName()}
+                 />
+                <button type="submit">Visit Store -></button>
+            </form>
+        )
+    }
+}
+
+export default StorePicker;
